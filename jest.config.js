832af47ef0012940280dc3f9bf9bf37432fbb4df
Radek Sedlák <sedlak.r@gmail.com>
2021-08-30 12:43:08 +0200

    adde junit reporter for tests

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    reporters: [
+        "default"
+        /*, "jest-junit"*/ //uncomment to run junit reporter
+    ]
 };
