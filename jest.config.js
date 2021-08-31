d2363784521a4b9159c6857ff81f9e90ef092d9c
Radek Sedlák <sedlak.r@gmail.com>
2021-08-31 11:48:15 +0200

    allow importing *.svg files in tsx and tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
+    transform: {
+        ".+\\.(css|styl|less|sass|scss|png|jpg|svg|ttf|woff|woff2)$": "jest-transform-stub"
+    },
     reporters: [
         "default"
         /*, "jest-junit"*/ //uncomment to run junit reporter
