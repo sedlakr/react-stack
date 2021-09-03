68fcad2e0c87ebba2e965d12759e3ffb8a07d311
Radek Sedlák <sedlak.r@gmail.com>
2021-09-03 09:18:02 +0200

    custom checkbox component in vendor

---
+++ src/vendor/ui/components/inputs/checkbox.tsx
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+
+export interface CheckboxProps {
+  isChecked?: boolean;
+}
+
+export function Checkbox({isChecked = false}: CheckboxProps) {
+  const [checked, setChecked] = useState(isChecked);
+  return (
+    <>
+      <input type={'checkbox'} checked={checked}
+             onClick={() => setChecked(!checked)}/>
+      {checked ? <div>checked</div> : <div>not checked</div>}
+    </>
+  );
+}
